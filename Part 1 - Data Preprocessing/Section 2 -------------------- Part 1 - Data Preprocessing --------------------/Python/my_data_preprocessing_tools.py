f705b550ecc8abd6f58d257f366a9a9e8dad7982
Aifert Yet <aifertyet0628@gmail.com>
2024-03-12 21:53:55 +0800

    finished for today

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Python/my_data_preprocessing_tools.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Python/my_data_preprocessing_tools.py
@@ -1,47 +1,47 @@
 #Importing the Libraries
-# import numpy as np
-# import matplotlib.pyplot as plt
-# import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
 # Construct the absolute path to the CSV file
 
 # Read the CSV file
 # dataset = pd.read_csv('/Users/aifertsmacbookpro/Desktop/Machine Learning A-Z (Codes and Datasets)/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Python/Data.csv')
-# dataset = pd.read_csv('Data.csv')
-# # Access the "Country" and "Purchased" columns
-# x = dataset.iloc[:, :-1].values
-# y = dataset.iloc[:, -1:].values
+dataset = pd.read_csv('Data.csv')
+# Access the "Country" and "Purchased" columns
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1:].values
 
 # Print the "Country" column
-# print(x)
-# print(y) 
+print(x)
+print(y) 
 
 
-# count = 0
-# for i in range(len(x) - 2):
-#     array = x[:, i+1]
-#     for value in array:
-#         if np.isnan(value):
-#             count += 1
-#     print(f"{count} number of missing data has been identified")
+count = 0
+for i in range(len(x) - 2):
+    array = x[:, i+1]
+    for value in array:
+        if np.isnan(value):
+            count += 1
+    print(f"{count} number of missing data has been identified")
 
-# from sklearn.impute import SimpleImputer
-# imputer = SimpleImputer(missing_values=np.nan, strategy= 'mean')
-# imputer.fit(x[:, 1:3])
-# x[:, 1:3] = imputer.transform(x[:, 1:3]) 
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy= 'mean')
+imputer.fit(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3]) 
 
-# from sklearn.compose import ColumnTransformer
-# from sklearn.preprocessing import OneHotEncoder
-# ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
-# x = np.array(ct.fit_transform(x))
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+x = np.array(ct.fit_transform(x))
 
-# from sklearn.preprocessing import LabelEncoder
-# le = LabelEncoder()
-# y = le.fit_transform(y)
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)
 
-# print(y)
+print(y)
 
-# print(x)
+print(x)
 # Importing the necessary libraries
 import pandas as pd 
 import numpy as np 
