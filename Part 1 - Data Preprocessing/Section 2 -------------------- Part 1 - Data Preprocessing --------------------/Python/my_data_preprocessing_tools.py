5f9898e693befb65cb7c52e2fb3e9bda7ad29ba1
Aifert Yet <aifertyet0628@gmail.com>
2024-03-14 11:41:50 +0800

    done feature scaling

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Python/my_data_preprocessing_tools.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Python/my_data_preprocessing_tools.py
@@ -1,88 +1,62 @@
-#Importing the Libraries
+# #Importing the Libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder, StandardScaler
+# # Construct the absolute path to the CSV file
 
-# Construct the absolute path to the CSV file
-
-# Read the CSV file
+# # Read the CSV file
 # dataset = pd.read_csv('/Users/aifertsmacbookpro/Desktop/Machine Learning A-Z (Codes and Datasets)/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Python/Data.csv')
 dataset = pd.read_csv('Data.csv')
 # Access the "Country" and "Purchased" columns
 x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1:].values
 
-# Print the "Country" column
-print(x)
-print(y) 
-
-
-count = 0
-for i in range(len(x) - 2):
-    array = x[:, i+1]
-    for value in array:
-        if np.isnan(value):
-            count += 1
-    print(f"{count} number of missing data has been identified")
-
 from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values=np.nan, strategy= 'mean')
 imputer.fit(x[:, 1:3])
 x[:, 1:3] = imputer.transform(x[:, 1:3]) 
 
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
 x = np.array(ct.fit_transform(x))
 
-from sklearn.preprocessing import LabelEncoder
-le = LabelEncoder()
-y = le.fit_transform(y)
-
-print(y)
-
-print(x)
-# Importing the necessary libraries
-import pandas as pd 
-import numpy as np 
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder
-
-# Load the dataset
-dataset = pd.read_csv('titanic.csv')
-x = dataset.iloc[:, :].values
-y = dataset.iloc[:, 1:2].values
-
-# Identify the categorical data
-# let ticket, fare cabin be the categorical data
-
-categorical_features = ['Sex', 'Embarked', 'Pclass']
-categorical_indicies = [dataset.columns.get_loc(feature) for feature in categorical_features]
-print(categorical_indicies)
-
-ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), categorical_indicies)], remainder="passthrough")
-x = np.array(ct.fit_transform(x))
-
-# Implement an instance of the ColumnTransformer class
 le = LabelEncoder()
 y = le.fit_transform(y)
 
 
-# Apply the fit_transform method on the instance of ColumnTransformer
 
+# # Identify the categorical data
+# # let ticket, fare cabin be the categorical data
 
-# Convert the output into a NumPy array
+# categorical_features = ['Sex', 'Embarked', 'Pclass']
+# categorical_indicies = [dataset.columns.get_loc(feature) for feature in categorical_features]
+# print(categorical_indicies)
 
+# ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), categorical_indicies)], remainder="passthrough")
+# x = np.array(ct.fit_transform(x))
 
-# Use LabelEncoder to encode binary categorical data
+# # Implement an instance of the ColumnTransformer class
+# le = LabelEncoder()
+# y = le.fit_transform(y)
 
+# print(x)
+# print(y).
 
-# Print the updated matrix of features and the dependent variable vector
+#Splitting the dataset into training set and test set
 
-print(x)
-print(y)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(x,y, test_size = 0.2, random_state = 1)
+# print(f"{X_train} X_train")
+# print(f"{X_test} X_test")
+# print(f"{y_train} y_train")
+# print(f"{y_test} y_test")
 
+#Feature Scaling
+scaler = StandardScaler()
 
+X_train[:,3:] = scaler.fit_transform(X_train[:,3:])
+X_test[:,3:] = scaler.transform(X_test[:,3:])
 
-
-
+print(X_train)
+print(X_test)
