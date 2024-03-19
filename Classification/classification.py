4d86f912ae49f7df1799d48b5b0840c5a2c0ea80
Aifert Yet <aifertyet0628@gmail.com>
2024-03-19 12:34:13 +0800

    done evaluating performance

---
+++ Classification/classification.py
@@ -0,0 +1,16 @@
+from logistic_regression import logistic
+from decision_tree_classification import decision_tree
+from k_nearest_neighbors import k_nearest
+from kernel_svm import kernel_svm
+from naive_bayes import naive
+from random_forest_classification import random_forest
+from support_vector_machine import support
+
+
+logistic()
+decision_tree()
+k_nearest()
+kernel_svm()
+naive()
+random_forest()
+support()
